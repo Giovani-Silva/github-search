cca35053e099aa0ff6ae76e7e60700df8c3a7f65
unknown <P51901401@brasilprev.com.br>
2021-02-21 12:13:00 -0300

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,7 @@ const api = axios.create({
   baseURL: 'https://api.github.com',
 });
 
-const token =
-  process.env.USER_SEARCH_OAUTH || 'f7b99201669fe98b8a9876324cce92ce7a22de2e';
+const token = process.env.USER_SEARCH_OAUTH;
 if (token) {
   api.defaults.headers.common.Authorization = `token ${token}`;
 }
